7aa04d2f95fd3fef76c1b5ca9007f602aec85659
JosiahKunz <jdkunz@anderson.edu>
2022-01-28 22:49:12 -0600

    moving to event-based
     - TypeName::OnTextChanged is the next one
     - Then, finish up ColorPicker
     - Then rename/categorize vars/functions
     - Then stress-test

--- GeneHunter/Source/GeneHunter/GeneHunterBPLibrary.cpp
+++ GeneHunter/Source/GeneHunter/GeneHunterBPLibrary.cpp
@@ -2,35 +2,42 @@
 #include "GeneHunterBPLibrary.h"
 #include "EditorScriptingUtilities/Public/EditorAssetLibrary.h"
 
-
+/*
+ * Determines a "safe" filename that is alphanumeric + "-" + "_" and is unique with the given Path. For example, takes "a 123 ??.txt" and returns "a123.txt".
+ *	- If it is not unique (for example, if there's already a file named "a123.txt"), counts up until it is unique (e.g., "a123_2.txt").
+ *	- It doesn't compare against itself, and hence the OldFilename is required.
+ */
 void UGeneHunterBPLibrary::EnsureValidAssetName(const FString Filename, const FString Path, FString& SafeFilename, FString& AbsolutePath, const FString Ext, const FString OldFilename)
 {
 	SafeFilename = "";
 
 	// Only alphanumeric characters allowed!
-	for (char c : Filename)
+	for (const char c : Filename)
 	{
-		if (std::isalnum(c))
+		if (std::isalnum(c) || c == '-' || c == '_')
 			SafeFilename += c;
 	}
 
-	// Make sure there aren't any duplicate asset names
-	int i=1;
-	FString NewFilename = SafeFilename;
-	AbsolutePath =
-			FPaths::ProjectContentDir() +
-			FPaths::ConvertRelativePathToFull(Path).Replace(TEXT("/Game/"), TEXT(""));
-	bool bFoundFile= FPaths::FileExists(AbsolutePath + "/" + NewFilename + "." + Ext);
-	while (bFoundFile && i<MAX_ITERATIONS)
+	// If the old name was already safe, by all means, keep it!
+	if (SafeFilename != OldFilename)
 	{
-		i += 1;
-		NewFilename = Filename + "_" + FString::FromInt(i);
-		if (NewFilename == OldFilename)
-			break;
-		bFoundFile= FPaths::FileExists(AbsolutePath + "/" + NewFilename + "." + Ext);
+		// Make sure there aren't any duplicate asset names
+		int i=1;
+		FString NewFilename = SafeFilename;
+		AbsolutePath =
+				FPaths::ProjectContentDir() +
+				FPaths::ConvertRelativePathToFull(Path).Replace(TEXT("/Game/"), TEXT(""));
+		bool bFoundFile= FPaths::FileExists(AbsolutePath + "/" + NewFilename + "." + Ext);
+		while (bFoundFile && i<MAX_ITERATIONS)
+		{
+			i += 1;
+			NewFilename = SafeFilename + "_" + FString::FromInt(i);
+			if (NewFilename == OldFilename)
+				break;
+			bFoundFile= FPaths::FileExists(AbsolutePath + "/" + NewFilename + "." + Ext);
+		}
+		SafeFilename = NewFilename;
 	}
-	SafeFilename = NewFilename;
-	
 }
 
 void UGeneHunterBPLibrary::SortAssetsAlphabetically(const TArray<FAssetData> Assets, TArray<FAssetData>& Sorted)
