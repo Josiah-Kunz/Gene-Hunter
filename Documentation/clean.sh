bb47b4382af03863b51bfaf219f8d20605e21404
JosiahKunz <jdkunz@anderson.edu>
2022-02-17 11:28:54 -0600

    Started docs

---
+++ Documentation/clean.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+rm *.log 2> /dev/null
+rm *.lof 2> /dev/null
+rm *.out 2> /dev/null
+rm *.synctex.gz 2> /dev/null
+rm *.aux 2> /dev/null
+rm *.bbl 2> /dev/null
+rm *.lot 2> /dev/null
+rm *.blg 2> /dev/null
+rm *.tdo 2> /dev/null
+rm *.toc 2> /dev/null
