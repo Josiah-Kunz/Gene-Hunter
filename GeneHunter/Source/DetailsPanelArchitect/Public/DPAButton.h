95d117d6902e5ce635b8ee3857a440bcb95ee171
JosiahKunz <jdkunz@anderson.edu>
2022-07-20 22:36:34 -0500

    basic class stable

---
+++ GeneHunter/Source/DetailsPanelArchitect/Public/DPAButton.h
@@ -0,0 +1,16 @@
+﻿// 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "DPAButton.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DETAILSPANELARCHITECT_API UDPAButton : public UObject
+{
+	GENERATED_BODY()
+};
