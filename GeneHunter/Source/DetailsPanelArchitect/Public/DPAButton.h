9020d97b58d516b08e31db512c933039af4156fb
JosiahKunz <jdkunz@anderson.edu>
2022-07-20 22:41:57 -0500

    variables compile

--- GeneHunter/Source/DetailsPanelArchitect/Public/DPAButton.h
+++ GeneHunter/Source/DetailsPanelArchitect/Public/DPAButton.h
@@ -4,6 +4,8 @@
 
 #include "CoreMinimal.h"
 #include "UObject/Object.h"
+#include "DetailCategoryBuilder.h"
+#include "DetailLayoutBuilder.h"
 #include "DPAButton.generated.h"
 
 /**
@@ -13,4 +15,50 @@ UCLASS()
 class DETAILSPANELARCHITECT_API UDPAButton : public UObject
 {
 	GENERATED_BODY()
+
+#pragma region Required public variables
+
+public:
+
+	/**
+	 * The label that appears on the left side of the column. Suggested constructor is LOCTEXT("ButtonLabel", "YourLabelHere").
+	 */
+	FText LabelText;
+	
+	std::function<void(void)>* OnClicked_Raw;
+
+#pragma endregion 
+
+#pragma region Optional public variables
+
+public:
+
+	/**
+	 * The text that appears over the button. The default value is LOCTEXT("ButtonText", "Go!").
+	 */
+	FText ButtonText;
+
+	/**
+	 * The category to either create or edit. Default is "Buttons".
+	 */
+	FName CategoryName;
+
+	/**
+	 *
+	 */
+	ECategoryPriority::Type CategoryPriority = ECategoryPriority::Important;
+	
+	/**
+	 * By default, this is LOCTEXT("CatName", CategoryName).
+	 */
+	FText LocalizedDisplayName;
+
+	/**
+	 * By default, this is LOCTEXT("Keyword", ButtonText).
+	 */
+	FText RowFilterString;
+
+	FSlateFontInfo LabelFont = IDetailLayoutBuilder::GetDetailFont();
+
+#pragma endregion
 };
