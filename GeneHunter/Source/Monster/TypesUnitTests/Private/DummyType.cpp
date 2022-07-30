cc3345193219d00b156e2d93ac8ea390bda4ac4d
JosiahKunz <jdkunz@anderson.edu>
2022-07-29 22:47:33 -0500

    Update DummyType.cpp

--- GeneHunter/Source/Monster/TypesUnitTests/Private/DummyType.cpp
+++ GeneHunter/Source/Monster/TypesUnitTests/Private/DummyType.cpp
@@ -15,27 +15,44 @@ void UDummyType::GetAllDummyAssets(TArray<FAssetData>& TypeAssets, const bool bS
 }
 
 /*
- * Gets all "Dummy" Types and converts them to UTypes. Note: these *MUST* be garbage collected manually when you're done!
+ * Gets all "Dummy" Types and replaces them with equivalent UTypes bound to the World.
+ * Note: these *MUST* be garbage collected manually when you're done, preferably with World->DestroyWorld(false) and
+ * CollectGarbage( GARBAGE_COLLECTION_KEEPFLAGS ) 
  * @param Types The returned array filled with Types found in the assets (see GetAllTypeAssets).
  * @param Exclude A list of Types to exclude from this list.
  * @param SortABC If true, sorts the Types alphabetically. Make false to improve performance.
  */
 void UDummyType::GetAllDummyTypes(UWorld* World, TArray<UType*>& Types, const TArray<UType*> Exclude, const bool bSortABC)
 {
+
+	// Get assets
 	Types.Empty();
 	TArray<FAssetData> Assets;
 	GetAllDummyAssets(Assets, bSortABC);
-	UObject* Outer = (UObject*)GetTransientPackage();
+	TArray<UDummyType*> DummyTypes;
+
+	// Create UType equivalents bound to World
 	for(FAssetData& Asset : Assets)
 	{
-		if (UType* Type = Cast<UType>(Asset.GetAsset()))
+		if (UDummyType* DummyType = Cast<UDummyType>(Asset.GetAsset()))
 		{
-			if (!Exclude.Contains(Type))
+			if (!Exclude.Contains(DummyType))
 			{
 				UType* NewType = NewObject<UType>(World);
-				NewType->AttackModifiers = Type->AttackModifiers;
+				NewType->Rename(*DummyType->GetName(), World);
+				//NewType->AttackModifiers = Type->AttackModifiers;
 				Types.Add(NewType);
+				DummyTypes.Add(DummyType);
 			}
+		}
+	}
+
+	// Update attack modifiers
+	for(int i=0; i<Types.Num();i++)
+	{
+		for(const TPair<UType*, FAttackModifier>& Pair : DummyTypes[i]->AttackModifiers)
+		{
+			Types[i]->AttackModifiers.Add(GetDummyTypeByName(Types,  FName(*Pair.Key->GetName())), Pair.Value);
 		}
 	}
 }
