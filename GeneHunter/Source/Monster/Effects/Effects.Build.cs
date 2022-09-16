8e00a68721d271139b7da87c1fbae82149b14b59
JosiahKunz <jdkunz@anderson.edu>
2022-09-15 21:41:27 -0500

    started effects

---
+++ GeneHunter/Source/Monster/Effects/Effects.Build.cs
@@ -0,0 +1,22 @@
+﻿using UnrealBuildTool;
+
+public class Effects : ModuleRules{
+
+	public Effects(ReadOnlyTargetRules Target) : base(Target){
+		
+		PrivateIncludePaths.Add("Monster/Effects/Private");
+		
+		PublicIncludePaths.Add("Monster/Effects/Public");
+		
+		PublicDependencyModuleNames.AddRange(new string[]{
+		}); 
+		
+		PrivateDependencyModuleNames.AddRange(new string[]{
+			
+			// Basics
+			"Core",			// for IMPLEMENT_MODULE
+			"CoreUObject",	// for UObjects
+			"Engine",		// for DataAsset.h
+		});
+	}
+}
