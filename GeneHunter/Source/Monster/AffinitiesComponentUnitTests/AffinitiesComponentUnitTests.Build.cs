26ba91fc2ccb4762e898609ee93419cff1450098
JosiahKunz <jdkunz@anderson.edu>
2023-02-13 14:39:21 -0600

    created affinitiescomponentunittests module

---
+++ GeneHunter/Source/Monster/AffinitiesComponentUnitTests/AffinitiesComponentUnitTests.Build.cs
@@ -0,0 +1,16 @@
+﻿using UnrealBuildTool;
+
+public class AffinitiesComponentUnitTests : ModuleRules{
+	
+	public AffinitiesComponentUnitTests(ReadOnlyTargetRules Target) : base(Target){
+		
+		PrivateIncludePaths.Add("Monster/StatsComponentUnitTests/Private");
+		
+		PrivateDependencyModuleNames.AddRange(new string[]{
+			"Core"						// for FString
+		  , "CoreUObject"				// for UObjects (and their initializations)
+		  , "Engine"					// for UBlueprintFuncitonLibrary in StatUnitTestUtilities.h
+		  , "AffinitiesComponent"		// b/c these are the thing we're testing
+		});
+	}
+}
