9464784e3eb503af5fcc81bd0b809d841482ddd6
JosiahKunz <jdkunz@anderson.edu>
2023-02-13 21:53:36 -0600

    #54 actually need to do all UEffectComponent issues first

---
+++ GeneHunter/Source/Monster/AffinitiesComponentUnitTests/Private/AffinitiesUnitTestUtilities.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+#include "AffinitiesComponent.h"
+#include "AffinitiesUnitTestUtilities.generated.h"
+
+UCLASS()
+class UAffinitiesUnitTestUtilities : public UBlueprintFunctionLibrary
+{
+public:
+
+	GENERATED_BODY()
+	
+	constexpr static float TOLERANCE = 0.1F;
+
+#define DUMMY_AFFINITIESCOMPONENT \
+	UWorld* DummyWorld = UWorld::CreateWorld(EWorldType::Game, false); \
+	AActor* DummyActor = DummyWorld->SpawnActor(AActor::StaticClass()); \
+	UAffinitiesComponent* AffinitiesComponent = NewObject<UAffinitiesComponent>(DummyActor); \
+	AffinitiesComponent->RegisterComponent(); 
+
+#define DUMMY_GC \
+	DummyWorld->DestroyWorld(false);
+	
+};
---
+++ GeneHunter/Source/Monster/AffinitiesComponentUnitTests/Private/Outlets/GetUnspentPoints.test.cpp
@@ -0,0 +1,45 @@
+﻿#pragma once
+
+#include "AffinitiesUnitTestUtilities.h"
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FGetUnspentPoints,
+	"__GeneHunter.Affinities Component.Outlets.GetUnspentPoints",
+	EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+
+bool FGetUnspentPoints::RunTest(const FString& Parameters)
+{
+
+	// Set up and give 3 unspent points
+	DUMMY_AFFINITIESCOMPONENT
+	AffinitiesComponent->SetUnspentPoints(3);
+
+	// Cache original to compare
+	const uint8 OriginalPoints = AffinitiesComponent->GetUnspentPoints();
+
+	// Attach effects TODO:
+	// One should be a double effect
+	// Another should be a zero effect
+	// Test order!
+
+	// Define delegate
+	FBeforeGetUnspentPointsDelegate DoublePointsDelegate;
+	DoublePointsDelegate.Delegate.BindDynamic()
+	
+
+	// Test that the points are doubled
+	const uint8 DoubledPoints = AffinitiesComponent->GetUnspentPoints();
+	TestEqual(
+			FString::Printf(
+			TEXT("UnspentPoints should be doubled: Original [%i] | Doubled [%i] (if zero, Priorities are messed up!)"),
+				OriginalPoints, DoubledPoints
+			),
+			2*OriginalPoints,
+			DoubledPoints,
+			UAffinitiesUnitTestUtilities::TOLERANCE);
+
+	// GC
+	DUMMY_GC
+	
+	return true;
+}
+
